13ac1211e1947d12d5295ae4a7926ee636541654
Nonim Udara <nonimudara123@gmail.com>
2023-03-29 11:51:45 +0530

    update app.js

--- App.js
+++ App.js
@@ -68,7 +68,9 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }} >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
