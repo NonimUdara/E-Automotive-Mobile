f4ffd010a66eaefe1aaa0a1eb17f1d1a40593941
Nonim Udara <nonimudara123@gmail.com>
2023-04-09 22:46:52 +0530

    add comment

--- Screens/Garage.js
+++ Screens/Garage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Linking, TextInput } from 'react-native';
 
 //const UserDashboard = ({ navigation }) => {
-
+//
 const LocationForm = () => {
     const [latitude, setLatitude] = useState('');
     const [longitude, setLongitude] = useState('');
