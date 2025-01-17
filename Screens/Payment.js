846f5f96176ef086ac890dd7d825b18ae4511137
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2025-01-17 22:28:20 +0530

    update stripe publishable key and secret key

--- Screens/Payment.js
+++ Screens/Payment.js
@@ -5,9 +5,17 @@ import { StripeProvider } from "@stripe/stripe-react-native"
 
 export default function Payment({navigation}) {
   return (
-    <StripeProvider publishableKey="pk_test_51N1DBBIcQPaMmd0Kitgwpu1HRAZxuFKrdac53qE4alOJZ5GURZkFuDfcPmCqFVFdUHXjTyOuX5Um31ffNeBavZyz00pIOY6omo" >
+    // <StripeProvider publishableKey="pk_test_51N1DBBIcQPaMmd0Kitgwpu1HRAZxuFKrdac53qE4alOJZ5GURZkFuDfcPmCqFVFdUHXjTyOuX5Um31ffNeBavZyz00pIOY6omo" >
+    //   <StripePayment navigation={navigation}/>
+    //   {/* <StripeApp/> */}
+    // </StripeProvider>
+
+    //update stripe publishable key
+
+    <StripeProvider publishableKey="pk_test_51QiFa22NxTyhzQ4xy1JRcdt8dGHYCPQmb4IMvcUlX2HyZpJDFl4HnPektBP89FmPsc0ge4ONiTCWvMeabvAqHUDz00Mh9LV9qW" >
       <StripePayment navigation={navigation}/>
       {/* <StripeApp/> */}
     </StripeProvider>
+
   );
 }
